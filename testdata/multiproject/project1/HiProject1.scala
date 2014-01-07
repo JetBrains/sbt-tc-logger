9b6d9e84710dc220253eb341f3dffba49a841e89
Ekaterina Shliakhovetskaja <shkate@jetbrains.com>
2014-01-07 13:45:50 +0100

    add multi project example as test data

---
+++ testdata/multiproject/project1/HiProject1.scala
@@ -0,0 +1,3 @@
+object HiProject1 {
+  def main(args: Array[String]) = 111println("Hi from Project 1!")
+}
---
+++ testdata/multiproject/project2/HiProject2.scala
@@ -0,0 +1,5 @@
+package multiproject.project2
+
+object HiProject2 {
+  def main(args: Array[String]) = 222println("Hi from Project 2!")
+}
