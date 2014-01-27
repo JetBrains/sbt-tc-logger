3af29120cb74069c1d61c6cd504caa4875f194e9
Ekaterina Shliakhovetskaja <shkate@jetbrains.com>
2014-01-27 02:30:29 +0100

    refactoring

--- src/main/scala/jetbrains/buildServer/TCLogAppender.scala
+++ src/main/scala/jetbrains/buildServer/TCLogAppender.scala
@@ -5,10 +5,12 @@ import jetbrains.buildServer.messages.serviceMessages.MapSerializerUtil
 
 class TCLogAppender extends LogAppender {
 
+  val CompilerName = "Scala compiler"
+
   def log(level: sbt.Level.Value, message: => String, flowId: String) = {
     val status = discoverStatus(level)
     val escaped = MapSerializerUtil.escapeStr(message, MapSerializerUtil.STD_ESCAPER2)
-    println(s"##teamcity[message status='$status' flowId='$flowId' text='$escaped']")
+    printServerMessage("message", Map("status" -> s"$status", "text" -> s"$escaped", "flowId" -> s"$flowId"))
 
   }
 
@@ -22,38 +24,45 @@ class TCLogAppender extends LogAppender {
   }
 
   def compilationBlockStart() {
-    println(s"##teamcity[compilationStarted compiler='Scala compiler']")
-  }
-
-  def compilationTestBlockStart() {
-      println(s"##teamcity[compilationStarted compiler='Scala compiler (in tests)']")
+    printServerMessage("compilationStarted", Map("compiler" -> CompilerName))
   }
 
   def compilationBlockEnd() {
-      println(s"##teamcity[compilationFinished compiler='Scala compiler']")
+    printServerMessage("compilationFinished", Map("compiler" -> CompilerName))
+  }
+
+  def compilationTestBlockStart() {
+      printServerMessage("compilationStarted", Map("compiler" -> s"$CompilerName in Test"))
   }
 
   def compilationTestBlockEnd() {
-      println(s"##teamcity[compilationFinished compiler='Scala compiler (in tests)']")
+    printServerMessage("compilationFinished", Map("compiler" -> s"$CompilerName in Test"))
   }
 
 
   def testSuitStart(name: String, flowId: String) {
-    println(s"##teamcity[testSuiteStarted name='$name' flowId='$flowId']")
+    printServerMessage("testSuiteStarted", Map("name" -> s"$name", "flowId" -> s"$flowId"))
   }
 
   def testOccurred(name: String, status: String, duration: Long, flowId: String) {
-    println(s"##teamcity[testStarted name='$name' captureStandardOutput='true' flowId='$flowId']")
-    println(s"##teamcity[testFinished name='$name' duration='$duration' flowId='$flowId']")
+    printServerMessage("testStarted", Map("name" -> s"$name", "captureStandardOutput" -> "true", "flowId" -> s"$flowId"))
+    printServerMessage("testFinished", Map("name" -> s"$name", "duration" -> "$duration", "flowId" -> s"$flowId"))
   }
 
   def testSuitSuccessfulResult(name: String, flowId: String) {
-    println(s"##teamcity[testSuiteFinished name='$name' flowId='$flowId']")
+    printServerMessage("testSuiteFinished", Map("name" -> s"$name", "flowId" -> s"$flowId"))
   }
 
   def testSuitFailResult(name: String, t: Throwable, flowId: String) {
-    val message = MapSerializerUtil.escapeStr(t.getMessage,MapSerializerUtil.STD_ESCAPER2)
+    val message = MapSerializerUtil.escapeStr(t.getMessage, MapSerializerUtil.STD_ESCAPER2)
     val details = t.getStackTrace
-    println(s"##teamcity[testSuiteFinished name='$name' message='$message' details='$details' flowId='$flowId']")
+    printServerMessage("testSuiteFinished", Map("name" -> s"$name", "message" -> s"$message", "details" -> s"$details","flowId" -> s"$flowId"))
+
+  }
+
+  private def printServerMessage(name: String, params: Map[String, String]) {
+    printf(s"##teamcity[$name")
+    for ((k, v) <- params) printf(s" $k='$v'")
+    println("]")
   }
 }
