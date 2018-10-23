967eecd199fbe2f3fc978496c7d6bd39fdc603c0
Justin Kaeser <justin.kaeser@jetbrains.com>
2018-10-23 14:59:56 +0200

    a bit more readable test definitions

--- test/src/jetbrains/buildServer/sbtlogger/SbtLoggerOutputTest_1_0.java
+++ test/src/jetbrains/buildServer/sbtlogger/SbtLoggerOutputTest_1_0.java
@@ -27,116 +27,121 @@ public class SbtLoggerOutputTest_1_0 {
 
     @Test
     public void testCompileErrorOutput() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("compile", new File("test/testdata/1.0/compileerror").getAbsolutePath());
+        SbtProcess.runAndTest("compile", testPath("compileerror"));
 
     }
 
     @Test
     public void testPluginStatus() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("sbt-teamcity-logger", new File("test/testdata/1.0/compileerror").getAbsolutePath(), "plugin_status_output.txt");
+        SbtProcess.runAndTest("sbt-teamcity-logger", testPath("compileerror"), "plugin_status_output.txt");
 
     }
 
     @Test
     public void testCompileSuccessfulOutput() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("compile", new File("test/testdata/1.0/compilesuccessful").getAbsolutePath());
+        SbtProcess.runAndTest("compile", testPath("compilesuccessful"));
 
     }
 
     @Test
     public void testMultiProjectsOutput() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("compile", new File("test/testdata/1.0/multiproject").getAbsolutePath());
+        SbtProcess.runAndTest("compile", testPath("multiproject"));
     }
 
 
     @Test
     public void testTmp() throws IOException, InterruptedException {
-        SbtProcess.runAndTestWithAdditionalParams("--debug", "compile", new File("test/testdata/1.0/multiproject").getAbsolutePath());
+        SbtProcess.runAndTestWithAdditionalParams("--debug", "compile", testPath("multiproject"));
     }
 
     /*@Test
     public void testScalaTest() throws IOException, InterruptedException {
-        int exitCode = SbtProcess.runAndTest("test", new File("test/testdata/1.0/testsupport/scalatest").getAbsolutePath(), "output.txt", "output1.txt");
+        int exitCode = SbtProcess.runAndTest("test", testPath("testsupport/scalatest"), "output.txt", "output1.txt");
         //if need exit code equals 0, otherwise in TeamCity additional non-informative build problem message will appear
         Assert.assertEquals(0, exitCode);
     }*/
 
     //todo[shkate]
     public void testWarningInTestOutput() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("test", new File("test/testdata/1.0/TW35693").getAbsolutePath());
+        SbtProcess.runAndTest("test", testPath("TW35693"));
     }
 
     @Test
     public void testTW35404_error() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("compile", new File("test/testdata/1.0/TW35404_error").getAbsolutePath());
+        SbtProcess.runAndTest("compile", testPath("TW35404_error"));
     }
 
     @Test
     public void testTW35404_debug() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("compile", new File("test/testdata/1.0/TW35404_debug").getAbsolutePath());
+        SbtProcess.runAndTest("compile", testPath("TW35404_debug"));
     }
 
     @Test
     public void testSubProject_compile() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("backend/compile", new File("test/testdata/1.0/subproject").getAbsolutePath());
+        SbtProcess.runAndTest("backend/compile", testPath("subproject"));
     }
 
     @Test
     public void testRunTestWithSbt() throws IOException, InterruptedException {
-        int exitCode = SbtProcess.runAndTest("test", new File("test/testdata/1.0/testsupport/scalatest").getAbsolutePath(), "output.txt", "output1.txt");
+        int exitCode = SbtProcess.runAndTest("test", testPath("testsupport/scalatest"), "output.txt", "output1.txt");
         //if need exit code equals 0, otherwise in TeamCity additional non-informative build problem message will appear
         Assert.assertEquals(0, exitCode);
     }
 
     //todo[shkate]
     public void testRunWithPluginFromBintray() throws IOException, InterruptedException {
-        SbtProcess.runWithoutApplyAndTest("test", new File("test/testdata/1.0/bintray").getAbsolutePath());
+        SbtProcess.runWithoutApplyAndTest("test", testPath("bintray"));
     }
 
 
     //todo[shkate]
     public void testRunWithPluginFromBintrayWithReApply() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("test", new File("test/testdata/1.0/bintray").getAbsolutePath());
+        SbtProcess.runAndTest("test", testPath("bintray"));
     }
 
     @Test
     public void testProjectWithJavaSources() throws IOException, InterruptedException {
-        SbtProcess.runAndTestWithAdditionalParams("clean compile run", "--debug", new File("test/testdata/1.0/withJavaSources").getAbsolutePath(), "output.txt");
+        SbtProcess.runAndTestWithAdditionalParams("clean compile run", "--debug", testPath("withJavaSources"), "output.txt");
     }
 
     //todo[shkate]
     public void testIgnoredTest() throws IOException, InterruptedException {
-        SbtProcess.runAndTestWithAdditionalParams("--info", "test", new File("test/testdata/1.0/ignoredTest").getAbsolutePath());
+        SbtProcess.runAndTestWithAdditionalParams("--info", "test", testPath("ignoredTest"));
     }
 
     //todo[shkate]
     public void testNestedSuites() throws IOException, InterruptedException {
-        SbtProcess.runAndTestWithAdditionalParams("--info", "test", new File("test/testdata/1.0/testsupport/nested").getAbsolutePath());
+        SbtProcess.runAndTestWithAdditionalParams("--info", "test", testPath("testsupport/nested"));
     }
 
     @Test
     public void testSpecTW46964() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("testOnly", new File("test/testdata/1.0/testsupport/scalatest_TW46964").getAbsolutePath(), "output.txt");
+        SbtProcess.runAndTest("testOnly", testPath("testsupport/scalatest_TW46964"), "output.txt");
     }
 
 
     //todo[shkate]
     public void testSpec2() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("testOnly", new File("test/testdata/1.0/testsupport/spec2").getAbsolutePath(), "output.txt");
+        SbtProcess.runAndTest("testOnly", testPath("testsupport/spec2"), "output.txt");
     }
 
     //todo[shkate]
     public void testTW50753_initErrorInTests() throws IOException, InterruptedException {
-        SbtProcess.runAndTest("clean compile test", new File("test/testdata/1.0/TW-50753_initErrorInTests").getAbsolutePath(), "output.txt");
+        SbtProcess.runAndTest("clean compile test", testPath("TW-50753_initErrorInTests"), "output.txt");
     }
 
 
     @Test
     public void testParallelTestExecutionTW43578() throws IOException, InterruptedException {
         SbtProcess.runAndTestWithAdditionalParams("--info", "test",
-                new File("test/testdata/1.0/testsupport/parallelTestExecutionTW43578/src/").getAbsolutePath(),
+                testPath("testsupport/parallelTestExecutionTW43578/src/"),
                 "output.txt", "output1.txt", "output2.txt", "output3.txt", "output4.txt", "output5.txt", "output7.txt",
                 "output6.txt", "output8.txt", "output9.txt", "output10.txt", "output11.txt");
+    }
+
+    private String testPath(String testRepo) {
+        File testDir = new File("test/testdata/1.0/", testRepo);
+        return testDir.getAbsolutePath();
     }
 
 }
